5636070d68c75a09512b26fb090406af932f5fab
Christian Boutin <christian.boutin@gmail.com>
2018-10-11 19:39:59 -0400

    Indent.

--- tictactoe/Assets/Scripts/AI.cs
+++ tictactoe/Assets/Scripts/AI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // An AI Necessarily plays for the O side
-public abstract class AI {
+public abstract class AI 
+{
 	abstract public void Play(GridLogic gridLogic, out int x, out int y);
 }
